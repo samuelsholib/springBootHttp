97360eb21aa3920752f7763950caf979cef19051
samuelsholib <samuelsholib12@gmail.com>
2022-09-06 00:25:16 -0700

    initial commit

---
+++ springMicroServices/src/main/java/springAppexample/springMicroServices/SpringMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package springAppexample.springMicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroServicesApplication.class, args);
+	}
+
+}
